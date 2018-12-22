b23cec59b72a372d47d142da169531af2859c5de
tom <tom@that-place>
2018-12-22 00:35:26 -0600

    REvised

--- src/aoc2018/ten/faster.clj
+++ src/aoc2018/ten/faster.clj
@@ -1,29 +1,18 @@
 (ns aoc2018.ten.faster
   (:require [aoc2018.ten :as ten]))
 
-;;Let's avoid intermediate vectors...
-;;14ms...
-(time
- (let [x 1 y 2]
-   (dotimes [i 1000000] [x y] )))
+;;The majority of optimizations here are
+;;going to unroll our destructuring,
+;;instead using type-hints on the vector
+;;for clojure.lang.Indexed, an then
+;;use direct method access
+;;(via the .nth form on the hinted
+;; object) to generate more efficient
+;;bytecode and save on function calls.
 
-
-;;13 ms
-(defn pair [x y]
-  (.cons  (clojure.lang.PersistentList. x ) y ))
-
-(time
- (let [x 1 y 2] (dotimes [i 1000000] (pair x y))))
-
-
-;;for the record, if we use structural types
-;;with direct field access, we get dis...
-(defrecord position [x y])
-
-;;9.6 ms, better!
-(time
- (let [x 1 y 2]
-   (dotimes [i 1000000] (position. x y))))
+;;We also use unchecked-math...
+;;but I put in unchecked-add|subtract
+;;variants for simplicity.
 
 ;;unroll using
 ;;~36-38ms vs 50ms @ 10^6 iterations.
@@ -63,30 +52,10 @@
         ymax (.nth bnds 3)]
   (unchecked-subtract ^long ymax ^long ymin)))
 
-;;166 ms
-#_(defn bounds [state]
-  (transduce (drop 1)
-             (completing
-              (fn [^clojure.lang.Indexed acc ^clojure.lang.Indexed pv]
-                (let [^clojure.lang.Indexed p (.nth pv 0)
-                      x (.nth p 0)
-                      y (.nth p 1)
-                      xmin  (.nth acc 0)
-                      xmax  (.nth acc 1)
-                      ymin  (.nth acc 2)
-                      ymax  (.nth acc 3)
-                      ]
-                  [(min xmin x)
-                   (max xmax x)
-                   (min ymin y)
-                   (max ymax y)])))
-              (let [[x y] (ffirst state)]
-                [x x y y]) state))
-
 
 ;;same
 (def evolve-state #(into [] (map evolve-point) %))
-
+  
 ;;same
 ;;no longer hold onto the head.
 (defn state-seq
@@ -132,8 +101,8 @@
 ;;This is now as fast as the
 ;;looped, non-hinted version from before.
 (time
- (shortest-state observations))
-;;"Elapsed time: 451.000849 msecs"
+ (shortest-state ten/observations))
+;;"Elapsed time: 442.000849 msecs"
 
 
 ;;This is now in the ballpark of CL
@@ -141,5 +110,107 @@
 
 ;;Clojure comes in around 283 to 296
 (time
- (shortest-state-loop observations))
+ (shortest-state-loop ten/observations))
 ;;"Elapsed time: 287.974153 msecs"
+
+
+
+
+;;EXPERIMENTS / MICROBENCHMARKS:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;Just some microbenchmarks to see
+;;if allocating intermediate vectors
+;;is costly, and if there are other
+;;options for optimization:
+
+(comment 
+;;Let's avoid intermediate vectors...
+;;14ms...
+(time
+ (let [x 1 y 2]
+   (dotimes [i 1000000] [x y] )))
+
+
+;;13 ms
+(defn pair [x y]
+  (.cons  (clojure.lang.PersistentList. x ) y ))
+
+(time
+ (let [x 1 y 2] (dotimes [i 1000000] (pair x y))))
+
+
+;;for the record, if we use structural types
+;;with direct field access, we get dis...
+(defrecord position [x y])
+
+;;9.6 ms, better!
+(time
+ (let [x 1 y 2]
+   (dotimes [i 1000000] (position. x y))))
+)
+
+
+;;166 ms
+#_(defn bounds [state]
+  (transduce (drop 1)
+             (completing
+              (fn [^clojure.lang.Indexed acc ^clojure.lang.Indexed pv]
+                (let [^clojure.lang.Indexed p (.nth pv 0)
+                      x (.nth p 0)
+                      y (.nth p 1)
+                      xmin  (.nth acc 0)
+                      xmax  (.nth acc 1)
+                      ymin  (.nth acc 2)
+                      ymax  (.nth acc 3)
+                      ]
+                  [(min xmin x)
+                   (max xmax x)
+                   (min ymin y)
+                   (max ymax y)])))
+              (let [[x y] (ffirst state)]
+                [x x y y]) state))
+
+
+;;slowe than reduce!
+#_(defn bounds-looped [^clojure.lang.PersistentVector state]
+  (let [bound  (count state)]
+    (loop [xmin   Long/MAX_VALUE
+           xmax   Long/MIN_VALUE
+           ymin   Long/MAX_VALUE
+           ymax   Long/MIN_VALUE
+           chunk  (chunk-first (seq state))
+           chunks (chunk-rest (seq state))
+           n      0
+           idx    0]
+      (cond (== n bound) [xmin xmax ymin ymax]
+            (< idx 32)             
+            (let [^clojure.lang.Indexed pv (.nth ^clojure.lang.ArrayChunk chunk idx)
+                  ^clojure.lang.Indexed p  (.nth pv 0)
+                  x (.nth p 0)
+                  y (.nth p 1)]
+              (recur 
+               (min xmin x)
+               (max xmax x)
+               (min ymin y)
+               (max ymax y)
+               chunk
+               chunks
+               (unchecked-inc n)
+               (unchecked-inc idx)))
+            :else
+            (recur xmin
+                   xmax
+                   ymin
+                   ymax
+                   (chunk-first chunks)
+                   (chunk-rest  chunks)
+                   n
+                   0)))))
+    
+;;no benefits really.
+#_(defn evolve-state2 [xs]
+  (persistent!
+   (reduce (fn [^clojure.lang.PersistentVector$TransientVector acc x]
+             (.conj acc (evolve-point x)))
+           (transient xs) xs)))
